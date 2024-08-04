69fcb3896fb505cf6a5dd449e652045855c0fe16
tanreaper <paulsapto@gmail.com>
2024-08-04 14:14:57 -0400

    + testing something

--- vars/gitHubSetup.groovy
+++ vars/gitHubSetup.groovy
@@ -4,18 +4,19 @@ def call(Map gitHubOption) {
         key, value ->
             println "${key} : ${value}"
     }
+    echo "${gitHubOption.GITHUB_REPO_NAME}"
 
-    sh """
-        curl -H "Authorization: token ${TOKEN}" \
-        -H "Content-Type: application/json" \
-        -d '{
-       "name": "${gitHubOption.GITHUB_REPO_NAME}",
-       "description": "${gitHubOption.REPO_DESCRIPTION}",
-       "private": "${gitHubOption.GITHUB_REPO_TYPE}",
-       "owner": "${gitHubOption.OWNER}"
-        }' \
-        {GITHUB_BASE_URL}/user/repos
-    """
+    // sh """
+    //     curl -H "Authorization: token ${TOKEN}" \
+    //     -H "Content-Type: application/json" \
+    //     -d '{
+    //    "name": "${gitHubOption.GITHUB_REPO_NAME}",
+    //    "description": "${gitHubOption.REPO_DESCRIPTION}",
+    //    "private": "${gitHubOption.GITHUB_REPO_TYPE}",
+    //    "owner": "${gitHubOption.OWNER}"
+    //     }' \
+    //     {GITHUB_BASE_URL}/user/repos
+    // """
 }
 
 def checkParams(Map gitHubOption) {
